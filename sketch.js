2276333619da0352bbe5a16b155aa55aa6d17613
Jai <31632266+jaip777@users.noreply.github.com>
2017-09-13 11:41:57 -0400

    First Sketch
    sketch.js
    index.html 
    ---> in p5 -> empty example

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(400,400);
+    console.log("Check, Check");
+}
+function draw(){
+    background(50,100);
+    fill(10,100,200);
+    ellipse(mouseX,mouseY, 50, 50,);
+}
